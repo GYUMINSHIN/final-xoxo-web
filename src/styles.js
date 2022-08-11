ef705f0dd6c1bcf4de4511f69ed698876d9c4d7c
GYUMINSHIN <92241822+GYUMINSHIN@users.noreply.github.com>
2022-08-11 11:16:57 +0900

    create: define variable

---
+++ src/styles.js
@@ -0,0 +1,19 @@
+export const colors = {
+    mint: "#1DFBB8",
+    lightMint: "#9DFEE0",
+    gray: "#B9B9B9",
+    darkGray: "#707070",
+    lightGray: "#F6F6F6",
+    blue: "#0095F6",
+    white: "#FFFFFF",
+};
+
+export const fontSize = {
+    XXL: "",
+    XL: "",
+    L: "",
+    M: "",
+    S: "",
+    XS: "",
+    XXS: "",
+}
