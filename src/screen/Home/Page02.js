1af673a6a765dc317318ea57dfd03c2a56405eb4
GYUMINSHIN <92241822+GYUMINSHIN@users.noreply.github.com>
2022-08-11 16:10:03 +0900

    create: make empty Page02,03,04,05

---
+++ src/screen/Home/Page02.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+	width: 100%;
+	height: 200px;
+	background-color: greenyellow;
+`;
+
+export default function Page02() {
+	return (
+		<Container>
+			
+		</Container>
+		
+	)
+}
