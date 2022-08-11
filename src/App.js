3902dc033b0aea0c1af8b098d1aa2067f017dd29
GYUMINSHIN <92241822+GYUMINSHIN@users.noreply.github.com>
2022-08-11 16:00:11 +0900

    create: use Route and Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Mainpage from "./screen/Home/Mainpage";
+import "./App.css";
 
 export default function App() {
-  return (
-      <div></div>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Mainpage />} />
+        </Routes>
+    );
 }
