5b49a50ed1f00733d8b4b53926a1b3f0b2682129
GYUMINSHIN <92241822+GYUMINSHIN@users.noreply.github.com>
2022-08-12 14:18:35 +0900

    create: make Page03

--- src/screen/Home/Page03.js
+++ src/screen/Home/Page03.js
@@ -1,16 +1,56 @@
 import React from "react";
 import styled from "styled-components";
+import tradeAfterChat from "../../Image/Home/page03-tradeAfterChat.svg";
+import moreInfo from "../../Image/Home/page03-moreInfo.svg";
+import cheering from "../../Image/Home/page03-cheering.svg";
+import arrowBtn from "../../Image/Home/page03-arrowBtn.svg";
 
 const Container = styled.div`
 	width: 100%;
-	height: 200px;
-	background-color: beige;
+	position: relative;
+	background-color: #2d2d2d;
+`;
+
+const TopBlackBox = styled.img`
+	width: 100%;
+`;
+
+const MiddleImg = styled.img`
+	width: 100%;
+`;
+
+const BottomBlackBox = styled.img`
+	width: 100%;
+`;
+
+const Btn = styled.button`
+	background-color: #2d2d2d;
+	border: 0px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	position: absolute;
+	bottom: 5vw;
+	right: 38vw;
+	cursor: pointer;
+	:hover{
+		opacity: 0.5;
+	}
+`;
+
+const ArrowBtn = styled.img`
+	width: 6vw;
 `;
 
 export default function Page03() {
 	return (
 		<Container>
-			
+			<TopBlackBox src={tradeAfterChat} />
+			<MiddleImg src={cheering} />
+			<BottomBlackBox src={moreInfo} />
+			<Btn>
+				<ArrowBtn src={arrowBtn} />
+			</Btn>
 		</Container>
 		
 	)
